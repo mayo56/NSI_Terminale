11156a0c5356dcb55e08800e517410e7fefebae1
Mayo <66140252+mayo56@users.noreply.github.com>
2023-11-24 10:56:23 +0000

    Ajout de la suite des methodes

--- Entrainement.py
+++ Entrainement.py
@@ -34,22 +34,56 @@ class ListeC:
         return self.tete is None
 
     def taille(self) -> int:
-        assert not self.est_vide(), "La liste ne doit pas être vide"
-
-        compteur = 1
-        element = self.tete
-        while element.suivant != None:
-            element = element.suivant
-            compteur += 1
-        return compteur
+        """
+        Renvoi la taille de la liste chainee
+        """
+        self._parcours_toute_la_chaine()[0]
     
     def get_dernier_maillon(self) -> Cellule:
-        assert not self.est_vide(), "La liste ne doit pas être vide."
+        """
+        Renvoi la dernière cellule
+        """
+        return self._parcours_toute_la_chaine()[1]
+    
+    def _parcours_toute_la_chaine (self, indice: int | None=None) -> list[int, Cellule]:
+        """
+        Methode mère pour parcourir la liste
+        """
+        assert not self.est_vide(), "La liste ne doit pas être vide"
 
+        compteur = 0
         element = self.tete
         while element.suivant != None:
+            if compteur == indice: # si i donnée
+                return [compteur, element]
             element = element.suivant
-        return element
+            compteur += 1
+        if compteur > indice: # Si l'indice donnée est plus grand que la liste
+            return [indice, None]
+        return [compteur + 1, element]
+
+    def get_maillon_indice(self, i:int) -> Cellule:
+        """
+        Renvoi le maillon selon l'indice
+        """
+        return self._parcours_toute_la_chaine(i)[1]
+
+    def ajout_debut(self, nM: any) -> None:
+        """
+        Attributs une nouvelle Cellule à la liste chainee
+        """
+        ma_cellule = Cellule(nM, self.tete)
+        self.tete = ma_cellule
+
+    def ajout_fin(self, nM:any) -> None:
+        element = Cellule(nM, None)
+        for _ in range(self.taille()):
+            dernier = self.get_dernier_maillon()
+            dernier.suivant = element
+            element = dernier
+        self.tete = element
+
+
 
 L = ListeC()
 M1, M2 = Cellule(21), Cellule(15)
@@ -57,3 +91,4 @@ M1.suivant = M2
 L.tete = M1
 print(L)
 print(L.get_dernier_maillon())
+print(L.get_maillon_indice(0))
